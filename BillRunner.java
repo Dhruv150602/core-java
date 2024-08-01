d72ec2ff94c0ffc99906c7a5c9380a773edd440f
Dhruv150602 <dhruvgroopesh@gmail.com>
2024-08-01 09:35:00 -0500

    commting the objects code

---
+++ BillRunner.java
@@ -0,0 +1,12 @@
+class BillRunner{
+         
+		 public static void main (String[] bill){
+               // Classwame rat = new ClassName () :
+            Bill ref = new Bill();
+            System.out.println(ref);
+            Bill ref1 = new Bill();
+            System.out.println(ref1);
+            Bill ref2 = new Bill();
+            System.out.println(ref2);
+}
+}
---
+++ Chair.java
@@ -0,0 +1,12 @@
+class Chair{
+        
+		public static void main{
+		
+		int id=10;
+		String uiId="10";
+		Integer chairId=60;
+		int concatRef=uid+id;
+		
+		}
+
+}
